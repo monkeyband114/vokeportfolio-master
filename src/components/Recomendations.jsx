0daa58d43eee323537f8722d7720be770c960c0a
monkeyband114 <gadocer54@gmail.com>
2025-06-22 20:15:43 +0100

    Changes to be committed: 	modified:   src/components/Recomendations.jsx
    Changes not staged for commit:
    	modified:   src/assets/projects/index.js
    	modified:   src/components/Projects/Category.jsx
    
    Untracked files:
    	src/assets/projects/dispatch.jpg
    	src/assets/projects/event.jpg
    	src/assets/projects/gamelearn.jpg

--- src/components/Recomendations.jsx
+++ src/components/Recomendations.jsx
@@ -50,7 +50,7 @@ const Recomendations = () => {
           title="Great Quality"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet...."
           pic={mike}
-          person="Chika Fula"
+          person="Shem Itoya"
           job="Grapic Designer"
         />
         <Rating
